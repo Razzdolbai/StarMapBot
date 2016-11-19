34ff8a899918a939b59cf808b49e810823d21f54
Razzdolbai <uarg75oye@gmail.com>
2016-11-19 10:48:51 +0300

    First commit

--- db.py
+++ db.py
@@ -11,6 +11,16 @@ try:
     con = lite.connect('bot.db')
 
     cur = con.cursor()
+
+    #Функция занесения вопроса в базу
+    def add_queestion(usrquestion,usranswer):
+        cur.execute("INSERT INTO questions (question,answer) VALUES ('%s','%s')"%(usrquestion,usranswer))
+        con.commit()
+    #Вводим данные
+    question = input("Введите Логин\n")
+    answer = input("Введите Пароль\n")
+    print('\n')
+
     cur.execute('SELECT id FROM questions')
 
     data = cur.fetchall()
